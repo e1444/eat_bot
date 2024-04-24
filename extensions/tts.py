d632d6e7dce61fffa39d8b90522d2e6284737100
Eric Liang <eliangca@outlook.com>
2024-04-24 16:08:16 -0700

    Removed redundancies in tts

--- extensions/tts.py
+++ extensions/tts.py
@@ -27,74 +27,16 @@ class TTSSource(discord.PCMVolumeTransformer):
     @classmethod
     def create_source(cls, ctx: discord.Interaction, url: str):
         return cls(ctx, discord.FFmpegPCMAudio(url, **cls.FFMPEG_OPTIONS), url=url)
-    
-    
-class MultiSource(discord.AudioSource):
-    def __init__(self, sources: list[discord.AudioSource], *, volume: float = 1):
-        self.sources: discord.AudioSource = sources
-        self._volume: float = volume
-        self._done: bool = False
-        
-    @property
-    def volume(self) -> float:
-        """Retrieves or sets the volume as a floating point percentage (e.g. ``1.0`` for 100%)."""
-        return self._volume
-
-    @volume.setter
-    def volume(self, value: float) -> None:
-        self._volume = max(value, 0.0)
-        
-    def add_source(self, new_source: discord.AudioSource):
-        self.sources.append(new_source)
-
-    def cleanup(self) -> None:
-        for source in self.sources:
-            source.cleanup()
-
-    def read(self) -> bytes:
-        ret = None
-        
-        for source in self.sources:
-            aret = source.read()
-            if aret:
-                if ret:
-                    ret = audioop.add(ret, aret, 2)
-                else:
-                    ret = aret
-        
-        if ret:
-            return audioop.mul(ret, 2, min(self._volume, 2.0))
-        else:   
-            self._done = True
-            return b''
-    
-    
-class VoiceState:
-    def __init__(self, bot: commands.Bot, ctx: discord.Interaction):
-        self._bot: commands.Bot = bot
-        self._ctx: discord.Interaction = ctx
-        self.voice: Optional[discord.VoiceClient] = None
-        self.source: Optional[MultiSource] = None
 
     
 class TTSCog(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot: Bot = bot
-        self.voice_states = {}
         
         print('---\tTTSCog loaded\t\t---')
         
     async def cog_app_command_error(self, ctx: discord.Interaction[discord.Client], error: app_commands.AppCommandError) -> None:
         await ctx.response.send_message('An error occurred: {}'.format(str(error)))
-        
-    def get_voice_state(self, ctx: discord.Interaction):
-        state = self.voice_states.get(ctx.guild_id)
-        
-        if not state:
-            state = VoiceState(self.bot, ctx)
-            self.voice_states[ctx.guild_id] = state
-
-        return state
         
     @app_commands.command(
         name='tts',
